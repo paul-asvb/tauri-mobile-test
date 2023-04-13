b542c7e495fc4825c97430047345f21054d59c7c
Paul Mayer <paul@paul-mayer.eu>
2023-04-13 13:55:52 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { internalIpV4 } from 'internal-ip'
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => {
+  const host = await internalIpV4()
+
+  /** @type {import('vite').UserConfig} */
+  const config = {
+    plugins: [vue()],
+    server: {
+      host: '0.0.0.0', // listen on all addresses
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        protocol: 'ws',
+        host,
+        port: 5183,
+      },
+    },
+  }
+
+  return config
+})
